c8a78f55a885bb97131eac745225e72fd7eaec2f
Prabhu Konchada <prabhukonchada@gmail.com>
2016-06-09 18:56:23 +0530

    Added a MovieDataModel with relavent getter and setter methods

---
+++ PopularMovies/app/src/main/java/com/prabhukonchada/popularmovies/MovieDataModel.java
@@ -0,0 +1,53 @@
+package com.prabhukonchada.popularmovies;
+
+/**
+ * Created by Prabhu Konchada on 09/06/16
+ * you can contact me at : prabhukonchada@gmail.com
+ */
+public class MovieDataModel {
+    private String movieName;
+    private String moviePosterImageThumbnail;
+    private String moviePlotSynopsis;
+    private String userRating;
+    private String releaseDate;
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMoviePosterImageThumbnail() {
+        return moviePosterImageThumbnail;
+    }
+
+    public void setMoviePosterImageThumbnail(String moviePosterImageThumbnail) {
+        this.moviePosterImageThumbnail = moviePosterImageThumbnail;
+    }
+
+    public String getMoviePlotSynopsis() {
+        return moviePlotSynopsis;
+    }
+
+    public void setMoviePlotSynopsis(String moviePlotSynopsis) {
+        this.moviePlotSynopsis = moviePlotSynopsis;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
