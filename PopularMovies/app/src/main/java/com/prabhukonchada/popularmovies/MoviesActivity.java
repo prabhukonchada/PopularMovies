723a313788010752100a26da92f9812e72d701c3
Prabhu Konchada <prabhukonchada@gmail.com>
2016-06-09 18:30:06 +0530

    Added fresco library to the project and initialised it

--- PopularMovies/app/src/main/java/com/prabhukonchada/popularmovies/MoviesActivity.java
+++ PopularMovies/app/src/main/java/com/prabhukonchada/popularmovies/MoviesActivity.java
@@ -3,12 +3,17 @@ package com.prabhukonchada.popularmovies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 public class MoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        // Initialise Fresco Library
+        Fresco.initialize(this);
 
         // Add MovieGridFragment to this activity
         getSupportFragmentManager().beginTransaction().add(R.id.container,new MovieGridFragment()).commit();
