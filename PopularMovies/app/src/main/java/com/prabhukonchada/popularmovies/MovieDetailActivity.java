882f3d3cc41391bfc6a27fd7927c63b599bd9e72
Prabhu Konchada <prabhukonchada@gmail.com>
2016-06-10 17:42:16 +0530

    Added skeleton of MovieDetailActivity to display movie details

---
+++ PopularMovies/app/src/main/java/com/prabhukonchada/popularmovies/MovieDetailActivity.java
@@ -0,0 +1,18 @@
+package com.prabhukonchada.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
