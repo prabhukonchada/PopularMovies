abef7d80f8e6e82e31c283d3758a39c03bc92349
Prabhu Konchada <prabhukonchada@gmail.com>
2016-06-13 12:18:39 +0530

    Changed model from serializable to parcelable

--- PopularMovies/app/src/main/java/com/prabhukonchada/popularmovies/MovieDataModel.java
+++ PopularMovies/app/src/main/java/com/prabhukonchada/popularmovies/MovieDataModel.java
@@ -1,18 +1,33 @@
 package com.prabhukonchada.popularmovies;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Prabhu Konchada on 09/06/16
  * you can contact me at : prabhukonchada@gmail.com
  */
-public class MovieDataModel implements Serializable {
+public class MovieDataModel implements Parcelable{
     private String movieName;
     private String moviePosterImageThumbnail;
     private String moviePlotSynopsis;
     private String releaseDate;
     private String movieImage;
     private String voteAverage;
+
+    public MovieDataModel()
+    {
+
+    }
+
+    protected MovieDataModel(Parcel in) {
+        movieName = in.readString();
+        moviePosterImageThumbnail = in.readString();
+        moviePlotSynopsis = in.readString();
+        releaseDate = in.readString();
+        movieImage = in.readString();
+        voteAverage = in.readString();
+    }
 
     public String getVoteAverage() {
         return voteAverage;
@@ -64,4 +79,32 @@ public class MovieDataModel implements Serializable {
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(movieName);
+        dest.writeString(moviePosterImageThumbnail);
+        dest.writeString(moviePlotSynopsis);
+        dest.writeString(releaseDate);
+        dest.writeString(movieImage);
+        dest.writeString(voteAverage);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<MovieDataModel> CREATOR = new Parcelable.Creator<MovieDataModel>() {
+        @Override
+        public MovieDataModel createFromParcel(Parcel in) {
+            return new MovieDataModel(in);
+        }
+
+        @Override
+        public MovieDataModel[] newArray(int size) {
+            return new MovieDataModel[size];
+        }
+    };
 }
